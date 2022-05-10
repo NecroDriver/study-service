dadc919089f7bdb44dc33d452637ed45a890425e
mafh <1193482868@qq.com>
2022-05-10 18:02:38 +0800

    first commit

---
+++ study-service-store/src/main/java/com/xin/store/StudyServiceStoreApplication.java
@@ -0,0 +1,13 @@
+package com.xin.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyServiceStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyServiceStoreApplication.class, args);
+    }
+
+}
---
+++ study-service-store/src/test/java/com/xin/store/StudyServiceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xin.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyServiceStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
